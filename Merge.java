a841a7ec9c795c9450899e7e099a17a66d67638a
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-02-14 19:39:37 +0000

    Merge sort method

---
+++ Merge.java
@@ -0,0 +1,13 @@
+public class Merge {
+    public void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+    }
+
+    public void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+    }
+
+    public static void sort(Comparable[] a) {
+	aux = new Comparable[a.length - 1];
+	sort(a, aux, 0, a.length - 1);
+	
+    }
+}
